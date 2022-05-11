e6b1769fedd9fa9636eba6990fee047f74a8f28c
jacobmensah11 <jacob.mensah@student.moringaschool.com>
2022-05-11 09:50:01 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=2;
+let num2=31;
 
-// Write your code here
+const multiply = num1*num2;
+console.log(multiply);
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+let num3=5;
+let num4=19;
+
+const mod=num4%num3;
+
+console.log(Math.max(1,5,7,10,19,20,4,6));
